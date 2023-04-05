7c00d712718f17ac00e0cdc6351854c3dcc54f21
Nathan Graebin <119326011+NathanGraebin@users.noreply.github.com>
2023-04-05 11:25:28 -0300

    Update Cliente.java

--- src/main/java/basicoPoo/Cliente.java
+++ src/main/java/basicoPoo/Cliente.java
@@ -4,25 +4,38 @@ package basicoPoo;
 import java.util.Scanner;
 
 public class Cliente {
-
+    //iniciaremos contruindo um Scanner para podermos dar valores as variaveis
     Scanner Ler = new Scanner(System.in);
-    private String nome;
-private int idCliente;
     
-
+    //Criaremos 2 variaveis PRIVADAS( podendo ser publicas tambem )
+    private String nome;
+    private int idCliente;
+    
+    //Faremos um construtor para o cliente, que seria passar as variaveis BASICAS para dizer que aquele é um CLIENTE
     public Cliente(String nome, int idCliente) {
         this.nome = nome;
         this.idCliente = idCliente;
     }
-
+    //aqui iniciaremos a contrução do metodo cadastrar cliente
     public void cadastrarCliente() {
+        //apresentaremos uma linha pedindo para informar o nome do cliente
         System.out.println("INFORME O NOME ");
+        
+        // setNome é responsavel por atribuir o nome do cliente,
+        // passaremos nosso scanner dentro dele, pois o metodo setNome precisa receber uma String, setNome(nome), então passaremos nosso Scanner para podermos
+        //definir o nome do cliente.  
+        // é como seu eu fizesse > setNome(Nathan Felipe)
         setNome(Ler.next());
-
+        
+        // da mesma forma faremos para o id, passaremos nosso Scanner dentro do setIdCliente afim de atribuir um valor para a variavel idCliente
+        // é como seu eu fizesse > setNome(Nathan Felipe)
         System.out.println("INFORME O ID ");
         setIdCliente(Ler.nextInt());
-
+        
+        
         System.out.println("CADASTRO REALIZADO");
+        
+        //apos isso o cadastro esta COMPLETO, finalizando e voltando para o menu
 
     }
 
